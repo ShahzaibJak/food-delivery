26c6c971350da1fdab39f85aab787e58bad7afd6
Shahzaib <shahzaibjak@gmail.com>
2023-07-07 11:54:50 -0700

    Database changes

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'food-delivery',
-        'HOST' : '127.0.0.1',
-        'PORT' : '3306',
-        'USER' : 'root',
-        'PASSWORD' : '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
